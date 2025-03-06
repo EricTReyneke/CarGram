2668123696b1b75ba2f607fc7b2d5609f91eaadb
EricTReyneke <eric2001reyneke@gmail.com>
2025-03-06 23:43:11 +0200

    Dashbaord heading change.

--- src/app/components/DashBoard.tsx
+++ src/app/components/DashBoard.tsx
@@ -5,7 +5,7 @@ import Styles from "../dashboard/Dashboard.module.css"
 export default function Lekker(){
 	return(
 		<div className={Styles.container}>
-            <h1 className={Styles.title}>Dashbaord Heading</h1>
+            <h1 className={Styles.title}>Welcome To CarGram</h1>
 		</div>
 	);
 }
